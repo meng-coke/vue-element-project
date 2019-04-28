a0be68d9c151c57c23270d8ddd94c23f41f3dbf1
e <liuxu@yunfucloud.com>
2019-04-28 19:30:45 +0800

    velg  增加路由，elementui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const loginModel = resolve => require(['@/views/loginModel/loginModel'],resolve)
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+const router = new Router({
+  routes:[
+    {path:'/',name:'loginModel',component:loginModel},
   ]
 })
+
+
+
+
+export default router
---
+++ src/views/loginModel/loginModel.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="guide-list">
+    <div>lalalal</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "login-model"
+    }
+</script>
+
+<style scoped>
+
+</style>
